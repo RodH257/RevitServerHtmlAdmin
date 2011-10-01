1a75ae13461c8ba3588016432354189192330731
RodH257 <rodhowarth257@gmail.com>
2011-10-01 22:30:22 +1000

    got folder retrieval working

---
+++ Default.aspx
@@ -0,0 +1,32 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Revit Sever HTML Admin</title>
+    <link href="StyleSheet.css" rel="stylesheet" type="text/css" />
+    <script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.6.4.min.js"></script>
+    <script src="RevitServer.js" type="text/javascript"></script>
+</head>
+<body>
+    <div id="top">
+        <img src="Images/RevitServerPic.PNG" />
+    </div>
+    <div id="left">
+        <div id="leftHeading">
+            <img src="Images/1CreateNew.png" id="CreateNewButton" />
+            <img src="Images/2Delete.png" id="DeleteButton" />
+            <img src="Images/3Cut.png" id="CutButton" />
+            <img src="Images/4Copy.png" id="CopyButton" />
+            <img src="Images/5Paste.png" id="PasteButton" />
+            <img src="Images/6Lock.png" id="LockButton" />
+        </div>
+        <div id="leftContent">
+            <span id="serverName"></span>
+            <br />
+            <ul id="Folders1">
+            </ul>
+        </div>
+    </div>
+    <div id="right">
+    </div>
+</body>
+</html>
---
+++ RevitServer.js
@@ -0,0 +1,92 @@
+﻿var SERVICE_URL = "http://localhost/RevitServerAdminRESTService/AdminRESTService.svc/";
+var RevitServer = {
+
+    //Revit Server API requires certain headers each time
+    //we'll store these here so we dont ahve to re-make them.
+    getCommonHeaders: function () {
+        var headers = {
+            'User-Name': 'Html5User',
+            'User-Machine-Name': 'localhost',
+            'Operation-GUID': Utils.getGuid() //GUID is used to identify each request.
+        };
+        return headers;
+    },
+
+    getServerInfo: function () {
+
+        //ajax call from Jquery
+        $.ajax({
+            url: SERVICE_URL + "serverProperties",
+            headers: RevitServer.getCommonHeaders(),
+            success: function (data) {
+                var serverNameText = data.CentralServerName;
+                if (data.IsCentralServer)
+                    serverNameText += ' (Central Server)';
+
+                $('#serverName').html(serverNameText);
+            },
+            error: function (data) {
+                alert('error' + data);
+            }
+        });
+
+    },
+
+    getFolders: function (currentPath, level) {
+
+
+        $.ajax({
+            url: SERVICE_URL + currentPath + '/contents',
+            headers: RevitServer.getCommonHeaders(),
+            success: function (data) {
+
+                for (var i = 0; i < data.Folders.length; i++) {
+                    var spacer = "";
+                    for (var x = 0; x < level; x++) {
+                        spacer += "&nbsp;";
+                    }
+
+                    $('#Folders1').append("<li><a href='#'>" + spacer + data.Folders[i].Name + "</a><ul></ul></li>");
+
+                    var path = data.Path.replace('\\', '|');
+                    if (path == " ")
+                        path = "";
+                    RevitServer.getFolders(path + '|' + data.Folders[i].Name, level + 1);
+                }
+
+            }
+        });
+
+    },
+
+    init: function () {
+
+        RevitServer.getServerInfo();
+        //start on folder " " which is the shortcut to get the base folders
+        RevitServer.getFolders(" ", 1);
+
+        $(function() {
+
+
+        });
+    }
+};
+
+
+var Utils = {
+    //taken from http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript
+    getGuid: function () {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    }
+};
+
+
+//entry point
+$(document).ready(function () {
+
+    RevitServer.init();
+
+});
